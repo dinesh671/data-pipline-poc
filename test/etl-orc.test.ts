5353feeefc0baccb96aee437af0f6699a5b28a8e
DvsDineshKumar <dinesh671@yahoo.com>
2026-04-20 22:51:27 +0530

    Initial commit

---
+++ test/etl-orc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EtlOrc from '../lib/etl-orc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/etl-orc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EtlOrc.EtlOrcStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
